c75e2db696e84574b2f27cac0261a82d38a36c81
Bruno <info@proyectarse.com>
2021-04-19 16:32:42 -0400

    CREATE: Screens

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import FormScreen from './src/screens/FormScreen';
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>App para muestra de form con hook</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+	return <FormScreen />;
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
