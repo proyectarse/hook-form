2920bf8f2d1708dc399fe4525e0194c4f70af844
Bruno <info@proyectarse.com>
2021-04-22 10:10:11 -0400

    Hook Form - Pruebas

--- src/screens/FormScreen.js
+++ src/screens/FormScreen.js
@@ -1,20 +1,132 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+	KeyboardAvoidingView,
+	Text,
+	StyleSheet,
+	Platform,
+	View,
+	TouchableWithoutFeedback,
+	ScrollView,
+	Keyboard,
+	TextInput,
+} from 'react-native';
+import { Button } from 'react-native-elements';
+import { useForm } from 'react-hook-form';
+import { InputText } from '../components';
 
-const FormScreen = () => (
-	<View style={styles.container}>
-		<Text>Hola Mundo</Text>
-		<StatusBar style="auto" />
-	</View>
-);
+const FormScreen = () => {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+		setValue,
+	} = useForm();
+	const onSubmit = (data) => console.log(data);
+
+	useEffect(() => {
+		fetchUser();
+	}, []);
+
+	const fetchUser = async () => {
+		try {
+			const res = await fetch(
+				`http://jsonplaceholder.typicode.com/users/1`
+			);
+			const { name, email } = await res.json();
+			setValue('name', name);
+		} catch (error) {}
+	};
+
+	return (
+		<View style={styles.screenContainerStyle}>
+			<StatusBar style="auto" />
+			<Text style={{ marginBottom: 20 }}>
+				Hola Mundo Lorem ipsum dolor sit amet consectetur adipisicing
+				elit. Cumque, natus. Cupiditate molestiae ipsum quidem nostrum
+				praesentium placeat temporatus. Cupiditate molestiae ipsum
+				quidem nostrum praesentium placeat temporibus quis voluptates,
+				sequi aliquam quas consequuntur, cumque corporis, vero aut
+				facilis aliquid.
+			</Text>
+			<ScrollView
+				style={{ paddingVertical: 20, backgroundColor: 'white' }}
+			>
+				<KeyboardAvoidingView
+					behavior={Platform.OS === 'ios' ? 'position' : 'padding'}
+					style={styles.container}
+					keyboardVerticalOffset={30}
+				>
+					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+						<View style={styles.innerForm}>
+							<InputText
+								errors={errors.name}
+								req={true}
+								defaultValue=""
+								labeltext="Nombre Completo"
+								placeholder="Ingrese nombre"
+								name="name"
+								control={control}
+							/>
+
+							<InputText
+								errors={errors.address}
+								req={false}
+								defaultValue=""
+								labeltext="Dirección"
+								placeholder="Ingrese Dirección"
+								name="address"
+								control={control}
+							/>
+
+							<InputText
+								errors={errors.rut}
+								req={false}
+								defaultValue=""
+								labeltext="Rut"
+								placeholder="Ingrese rut"
+								name="rut"
+								typeRut={true}
+								control={control}
+							/>
+
+							<InputText
+								errors={errors.component}
+								req={true}
+								defaultValue=""
+								labeltext="Componente"
+								placeholder="Ingrese Componente"
+								name="component"
+								control={control}
+							/>
+
+							<Button
+								title="Enviar"
+								type="outline"
+								onPress={handleSubmit(onSubmit)}
+							/>
+						</View>
+					</TouchableWithoutFeedback>
+				</KeyboardAvoidingView>
+			</ScrollView>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		paddingHorizontal: 10,
+	},
+	screenContainerStyle: {
+		paddingHorizontal: 10,
+		paddingTop: 40,
+		flex: 1,
+		backgroundColor: 'whitesmoke',
+	},
+	innerForm: {
+		flex: 1,
+		paddingVertical: 10,
 	},
 });
 
